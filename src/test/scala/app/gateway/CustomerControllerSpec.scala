5139b0da8d247ef4f52ea3b5901c829aa15562f9
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-01-01 11:27:48 +0100

    tests in progress 2

--- src/test/scala/app/gateway/CustomerControllerSpec.scala
+++ src/test/scala/app/gateway/CustomerControllerSpec.scala
@@ -14,7 +14,6 @@ import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
 import org.http4s.circe._
 import org.http4s.implicits._
 import zio._
-import zio.console.putStrLn
 import zio.interop.catz._
 import zio.test._
 
@@ -25,95 +24,111 @@ object CustomerControllerSpec extends DefaultRunnableSpec {
   implicit val app: Kleisli[CustomerTask, Request[CustomerTask], Response[CustomerTask]] =
     new CustomerController[CustomerServiceEnv]().routes("").orNotFound
 
-//  private val customerControllerSuite = suite("CustomerController")(
-//    createNewCustomerResponseTest,
-//    queryNewCustomerTest,
-//    deleteCustomerResponseTest,
-//    queryDeletedCustomerTest
-//  )
+  private val customerControllerSuite = suite("CustomerController")(
+    createNewCustomerResponseTest,
+    createNewCustomerThenQueryTest,
+    queryNonExistingCustomerTest,
+    createCustomersThenGetAllTest,
+    createCustomerThenDeleteResponseTest,
+    deleteNonExistingCustomerResponseTest,
+    deleteAllThenQueryTest
+  )
 
   override def spec =
-    suite("CustomerController")(
-      testM("create new customer and verify answer") {
-        val expectedResponse = Some(
-          json"""{
+    customerControllerSuite
+      .provideSomeLayer[ZEnv](DependencyConfig.inMemory.appLayer)
+
+  lazy val createNewCustomerResponseTest =
+    testM("create new customer and verify answer") {
+      val expectedResponse = Some(
+        json"""{
             "id": "1",
             "url": "/1",
             "name": "Test",
             "locked":false
           }""")
 
-        for {
-          response <- CustomerLifecycle.create(json"""{"name": "Test"}""")
-          bodyCheckResult <- checkBody(response, expectedResponse)
-        } yield checkStatus(response, Status.Created) && bodyCheckResult
-      },
-      testM("create new customer then query it") {
-        val expectedResponse = Some(
-          json"""{
+      for {
+        response <- CustomerLifecycle.create(json"""{"name": "Test"}""")
+        bodyCheckResult <- checkBody(response, expectedResponse)
+      } yield checkStatus(response, Status.Created) && bodyCheckResult
+    }
+
+  lazy val createNewCustomerThenQueryTest =
+    testM("create new customer then query it") {
+      val expectedResponse = Some(
+        json"""{
             "id": "1",
             "url": "/1",
             "name": "Test",
             "locked":false
           }""")
 
-        for {
-          createdResponse <- CustomerLifecycle.create(json"""{"name": "Test"}""")
-          customerId <- asApiOutput(createdResponse).map(_.id)
-          response <- CustomerLifecycle.getById(customerId)
-          bodyCheckResult <- checkBody(response, expectedResponse)
-        } yield checkStatus(response, Status.Created) && bodyCheckResult
-      },
-      testM("query non existing customer") {
-        for {
-          response <- CustomerLifecycle.getById("111")
-        } yield checkStatus(response, Status.NotFound)
-      },
-      testM("create two customers and then get all customers") {
-        val expectedResponse = Some(
-          json"""[
+      for {
+        createdResponse <- CustomerLifecycle.create(json"""{"name": "Test"}""")
+        customerId <- asApiOutput(createdResponse).map(_.id)
+        response <- CustomerLifecycle.getById(customerId)
+        bodyCheckResult <- checkBody(response, expectedResponse)
+      } yield checkStatus(response, Status.Created) && bodyCheckResult
+    }
+
+  lazy val queryNonExistingCustomerTest =
+    testM("query non existing customer") {
+      for {
+        response <- CustomerLifecycle.getById("111")
+      } yield checkStatus(response, Status.NotFound)
+    }
+
+  lazy val createCustomersThenGetAllTest =
+    testM("create two customers and then get all customers") {
+      val expectedResponse = Some(
+        json"""[
               {"id": "1", "url": "/1", "name": "Test", "locked":false},
               {"id": "2", "url": "/2", "name": "Test2", "locked":false}
             ]"""
-        )
+      )
 
-        for {
-          _ <- CustomerLifecycle.create(json"""{"name": "Test"}""")
-          _ <- CustomerLifecycle.create(json"""{"name": "Test2"}""")
-          response <- CustomerLifecycle.getAll
-          bodyCheckResult <- checkBody(response, expectedResponse)
-        } yield checkStatus(response, Status.Ok) && bodyCheckResult
-      },
-      testM("create customer then delete it by id and verify answer") {
-        val expectedResponse = Some(json"""[]""")
-        for {
-          response <- CustomerLifecycle.create(json"""{"name": "Test"}""")
-          customer <- asApiOutput(response)
-          customerId = customer.id
-          _ <- CustomerLifecycle.deleteById(customerId)
-          response <- CustomerLifecycle.getAll
-          bodyCheckResult <- checkBody(response, expectedResponse)
-        } yield checkStatus(response, Status.Ok) && bodyCheckResult
-      },
-      testM("should return not found when deleting non existing customer") {
-        val expectedResponse = Option.empty[String]
+      for {
+        _ <- CustomerLifecycle.create(json"""{"name": "Test"}""")
+        _ <- CustomerLifecycle.create(json"""{"name": "Test2"}""")
+        response <- CustomerLifecycle.getAll
+        bodyCheckResult <- checkBody(response, expectedResponse)
+      } yield checkStatus(response, Status.Ok) && bodyCheckResult
+    }
 
-        for {
-          response <- CustomerLifecycle.deleteById("1")
-          bodyCheckResult <- checkBody(response, expectedResponse)
-        } yield checkStatus(response, Status.NotFound) && bodyCheckResult
-      },
-      testM("should delete all customers") {
-        val expectedResponse = Some(json"""[]""")
-        for {
-          _ <- CustomerLifecycle.create(json"""{"name": "Test"}""")
-          _ <- CustomerLifecycle.create(json"""{"name": "Test2"}""")
-          _ <- CustomerLifecycle.deleteAll
-          response <- CustomerLifecycle.getAll
-          bodyCheckResult <- checkBody(response, expectedResponse)
-        } yield checkStatus(response, Status.Ok) && bodyCheckResult
-      }
-    ).provideSomeLayer[ZEnv](DependencyConfig.inMemory.appLayer)
+  lazy val createCustomerThenDeleteResponseTest =
+    testM("create customer then delete it by id and verify answer") {
+      val expectedResponse = Some(json"""[]""")
+      for {
+        response <- CustomerLifecycle.create(json"""{"name": "Test"}""")
+        customer <- asApiOutput(response)
+        customerId = customer.id
+        _ <- CustomerLifecycle.deleteById(customerId)
+        response <- CustomerLifecycle.getAll
+        bodyCheckResult <- checkBody(response, expectedResponse)
+      } yield checkStatus(response, Status.Ok) && bodyCheckResult
+    }
+
+  lazy val deleteNonExistingCustomerResponseTest =
+    testM("should return not found when deleting non existing customer") {
+      val expectedResponse = Option.empty[String]
+
+      for {
+        response <- CustomerLifecycle.deleteById("1")
+        bodyCheckResult <- checkBody(response, expectedResponse)
+      } yield checkStatus(response, Status.NotFound) && bodyCheckResult
+    }
+
+  lazy val deleteAllThenQueryTest = testM("should delete all customers") {
+    val expectedResponse = Some(json"""[]""")
+    for {
+      _ <- CustomerLifecycle.create(json"""{"name": "Test"}""")
+      _ <- CustomerLifecycle.create(json"""{"name": "Test2"}""")
+      _ <- CustomerLifecycle.deleteAll
+      response <- CustomerLifecycle.getAll
+      bodyCheckResult <- checkBody(response, expectedResponse)
+    } yield checkStatus(response, Status.Ok) && bodyCheckResult
+  }
 
   private def asApiOutput(response: Response[CustomerTask]): CustomerTask[CustomerApiOutput] = {
     implicit def circeJsonDecoder[A](implicit
