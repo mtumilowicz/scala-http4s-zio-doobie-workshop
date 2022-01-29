6a5fc440819aa1570f722e2e76dd1b41dd8aa64b
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-01-29 13:55:25 +0100

    automatically infering decoder/encoder with org.http4s.circe

--- src/main/scala/app/gateway/customer/CustomerController.scala
+++ src/main/scala/app/gateway/customer/CustomerController.scala
@@ -5,13 +5,13 @@ import app.domain.customer._
 import app.gateway.customer.out.CustomerApiOutput
 import app.infrastructure.config.customer.CustomerServiceProxy
 import io.circe.syntax.EncoderOps
-import io.circe.{Decoder, Encoder}
-import org.http4s._
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 import zio.interop.catz._
 import org.http4s.HttpRoutes
 import zio._
+import org.http4s.circe.CirceEntityEncoder._
+import org.http4s.circe.CirceEntityDecoder._
 //import zio.interop.catz.implicits._
 //
 //import java.util.concurrent.TimeUnit
@@ -25,12 +25,6 @@ class CustomerController[R <: CustomerServiceEnv] {
 
     val dsl: Http4sDsl[CustomerTask] = Http4sDsl[CustomerTask]
     import dsl._
-
-    implicit def circeJsonDecoder[A: Decoder]: EntityDecoder[CustomerTask, A] =
-      jsonOf[CustomerTask, A]
-
-    implicit def circeJsonEncoder[A: Encoder]: EntityEncoder[CustomerTask, A] =
-      jsonEncoderOf[CustomerTask, A]
 
     HttpRoutes.of[CustomerTask] {
       case GET -> Root / id =>
